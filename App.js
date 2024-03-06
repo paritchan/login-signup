a47515e9fe9c35c486c853de10527c6fcb383183
appyfizzz <31124703+appyfizzz@users.noreply.github.com>
2024-03-06 23:34:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { FaStar }  from "react-icons/fa";
+
+const colors = {
+orange:"#FFA500",
+grey:"#808080"
+}
+
+
+function App() {
+ const stars = Array(5).fill(0);
+ const [currentValue, setCurrentValue] = useState(0);
+ const [hoverValue,setHoverValue] = useState(undefined);
+
+
+const handleClick = value =>{
+  setCurrentValue(value)
+};
+
+const handleMouseOver = newHoverValue =>{
+  setHoverValue(newHoverValue)
+};
+
+const handleMouseLeave = () =>{
+  setHoverValue(undefined)
+}
+
+
+  return (
+      <div style={styles.container}>
+        <h2>User Feedback</h2>
+        <div style={styles.stars}>
+         {stars.map((_,index) => {
+           return (
+             <FaStar
+                key={index}
+                size={24}
+                style={{
+                  marginRight : 10,
+                  cursor:"pointer"
+                }}
+                color = {(hoverValue || currentValue)>index ? colors.orange : colors.grey}
+                onClick={()=> handleClick(index + 1 )}
+                onMouseOver={()=>handleMouseOver(index+1)}
+                onMouseLeave={handleMouseLeave}
+              />
+           )
+         })}
+        </div>
+        <textarea
+            placeholder = "Whats your Feedback"
+            style={styles.textarea}
+        />
+        <button style={styles.button}> Submit
+        </button>
+        </div>
+
+
+  )
+}
+
+const styles = {
+   container : {
+     display:"flex",
+     flexDirection:"column",
+     alignItems:"center",
+   },
+   textarea:{
+     border: "1px solid #a9a9a9",
+     borderRadius : 5,
+     width: 300,
+     margin:"20px 0",
+     minHeight: 100,
+     padding: 10
+   },
+   button:{
+     border: "1px solid #a9a9a9",
+     borderRadius : 5,
+     width: 300,
+     padding: 10
+   }
+};
+
+export default App;
